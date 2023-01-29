8390dc2435ebd834ec635a2135809c892e5db481
Trevor Cassell <tjcassell87@gmail.com>
2023-01-28 20:14:27 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Two arrays used to generate team names
+        string[] adjectives = { "Loyal", "Diligent", "Classy", "Team-Oriented", "Honest" };
+        string[] nouns = { "Employee", "Chilhowie Warrior", "Worker", "Golfer", "Stamp Collector" };
+
+        //Get random name
+        Random rnd = new Random();
+        int adjIndex = rnd.Next(adjectives.Length);
+        int nounIndex = rnd.Next(nouns.Length);
+        string teamName = adjectives[adjIndex] + " " + nouns[nounIndex];
+        Console.WriteLine(teamName);
+
+    }
+}
+;
